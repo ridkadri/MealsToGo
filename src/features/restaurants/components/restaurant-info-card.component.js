45093476c4483d5c8e6f576a0560fc9cc8db9a5f
Ridwan Kadri <kadriridwan0@gmail.com>
2021-03-18 00:01:15 +0000

    created infrastructure folder to hold font,colors, spacing and sizes. also created feature folder to hold the spacer and typography. added a star and open svg files

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,28 +1,98 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import styled from 'styled-components/native';
+import {Text, Image, View} from 'react-native';
 import { Card } from 'react-native-paper';
+import {SvgXml} from 'react-native-svg';
+import {Spacer} from './spacer/spacer.component';
+
+import star from '../../../../assets/star';
+import open from '../../../../assets/open';
+
+const RestaurantCard = styled(Card)`
+    color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+    padding: ${(props) => props.theme.space[3]}; 
+    color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const Address = styled.Text`
+    font-family: ${(props) => props.theme.fonts.body};
+    font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Title = styled.Text`
+    font-family: ${(props) => props.theme.fonts.heading};
+    font-size: ${(props) => props.theme.fontSizes.body};
+    color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Info = styled.View`
+    padding: ${(props) => props.theme.space[3]}; 
+`;
+
+const Rating = styled.View`
+    flex-direction: row; 
+    padding-top: ${(props) => props.theme.space[2]};
+    padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+const Section = styled.View`
+    flex-direction: row;
+    align-items: center;
+`;
+
+const SectionEnd = styled.View`
+    flex: 1;
+    flex-direction: row;
+    justify-content: flex-end;
+`;
+
+const Open = styled(SvgXml)`
+    flex-direction: row;
+`;
 
 export const RestaurantInfoCard = ({ restaurant = {}} ) => {
     const {
-        name = 'Some Restaurant', 
-        icon, 
-        photos = ["https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fkeyassets-p2.timeincuk.net%2Fwp%2Fprod%2Fwp-content%2Fuploads%2Fsites%2F53%2F2018%2F04%2Fbeetroot-burger.jpg&f=1&nofb=1"], 
-        address = 'Some address', 
+        name = 'Papaye', 
+        icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png", 
+        photos = ["https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fbeautifulghana.com%2Fwp-content%2Fuploads%2F2020%2F05%2Fgrill-thigh-rice-no-bg_600x325.png&f=1&nofb=1"], 
+        address = '100 Osu Oxford St', 
         isOpenNow = true, 
         rating = 4, 
-        isClosedTemporarily,
+        isClosedTemporarily = true,
     } = restaurant;
 
+    const ratingArray = Array.from(new Array(Math.floor(rating)));
+
     return (
-        <Card elevation={5} style={styles.card}>
-            <Card.Cover key={name} style={styles.cover} source={{uri: photos[0] }}/>
-            <Text style={styles.title}>{name}</Text>
-        </Card>
+        <RestaurantCard elevation={5}>
+            <RestaurantCardCover key={name} source={{uri: photos[0] }}/>
+            <Info>
+                <Title>{name}</Title>
+                <Section>
+                    <Rating>
+                        {ratingArray.map(()=> (
+                            <SvgXml xml={star} width={20} height={20} />
+                        ))}
+                    </Rating>
+                    <SectionEnd>
+                        {isClosedTemporarily && (
+                            <Text variant='label' style={{color: 'red'}}>
+                                CLOSED TEMPORARILY
+                            </Text>
+                        )}
+                        <Spacer position='left' size='large'>
+                            {isOpenNow && <Open xml={open} width={20} height={20} />}
+                        </Spacer>
+                        <Spacer position='left' size='large'>
+                            <Image style={{width: 15, height: 15}} source={{uri: icon}}/>
+                        </Spacer>
+                    </SectionEnd>
+                </Section>
+                <Address>{address}</Address>
+            </Info>
+        </RestaurantCard>
     )
 };
-
-const styles = StyleSheet.create({
-    card: {backgroundColor: 'white'},
-    cover: {padding: 20, backgroundColor: 'white'},
-    title: {padding: 16},
-});
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,30 +1,31 @@
 import React from 'react';
+import styled from 'styled-components/native';
 import { Searchbar } from 'react-native-paper';
-import {SafeAreaView, View, Text, StyleSheet, StatusBar} from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
+
 import { RestaurantInfoCard} from "../components/restaurant-info-card.component";
 
-export const RestaurantsScreen = () => (
-    <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-            <Searchbar/>
-        </View>
-        <View style={styles.list}>
-            <RestaurantInfoCard/>
-        </View>
-    </SafeAreaView>
-);
+const SafeArea = styled(SafeAreaView)`
+    flex: 1;
+    margin-top: ${StatusBar.currentHeight && `margin-top : ${StatusBar.currentHeight}px`};
+`;
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1, 
-        marginTop: StatusBar.currentHeight
-    },
-    search: {
-        padding: 20
-      },
-    list: {
-        flex: 1,
-        backgroundColor: 'blue',
-        padding: 20
-    },
-});
+const SearchContainer = styled.View`
+    padding: ${(props) => props.theme.space[3]};
+`;
+
+const RestaurantListContainer = styled.View`
+    flex: 1;
+    padding: ${(props) => props.theme.space[3]};
+`;
+
+export const RestaurantsScreen = () => (
+    <SafeArea>
+        <SearchContainer>
+            <Searchbar/>
+        </SearchContainer>
+        <RestaurantListContainer>
+            <RestaurantInfoCard/>
+        </RestaurantListContainer>
+    </SafeArea>
+);
